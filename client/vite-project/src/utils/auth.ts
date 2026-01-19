804b68189e0110af2161c755c0c3ca9a03853ec1
EliasEssawi <elias.essawi@e.braude.ac.il>
2026-01-19 10:55:01 +0200

    dhiuasbgdiuashdiuhasiud

--- client/vite-project/src/utils/auth.ts
+++ client/vite-project/src/utils/auth.ts
@@ -1,21 +1,20 @@
-import axios from "axios";
-const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
-       
+import api from "../api/axios"; // or "../../api/axios" depending on your setup
+
 export async function isLoggedIn(): Promise<boolean> {
   try {
-    await axios.get(`${API_BASE}/auth/authMe`, { withCredentials: true });
+    await api.get("/api/auth/authMe"); // baseURL comes from axios.ts
     return true;
   } catch {
     return false;
   }
 }
 
-export async function logout():Promise<boolean> {
-  try{
-    await axios.post(`${API_BASE}/auth/logout`,{},{ withCredentials: true }); // ✅ needed to send cookie);
+export async function logout(): Promise<boolean> {
+  try {
+    await api.post("/api/auth/logout", {}); // cookie sent automatically
     localStorage.clear();
     return true;
-  }catch {
+  } catch {
     return false;
   }
 }
